51cbc72aeabb8293e721f8eb912cb71aaa8209ed
bea <beatriztattooer@gmail.com>
2022-09-24 17:00:05 -0300

    :lipstick: component: footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,59 @@
-<div>
-    <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
+<div style="background: #00153D;color:white;">
+    <div class="container">
+      <footer class="row row-cols-5 py-5 mt-5 border-top">
+        <div class="col-12 col-md py-2 py-md-0">
+          <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+          <img src="{{asset('logo/emblema.png')}}">
+          </a>
+        </div>
+
+        <div class="col-12 col-md py-2 py-md-0">
+          <h5>Matrículas</h5>
+        </div>
+
+        <div class="col-12 col-md py-2 py-md-0">
+          <h5>Institucional</h5>
+        </div>
+    
+        <div class="col-12 col-md py-2 py-md-0">
+          <h5>Renovação KIDS</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-muted">
+                Rua Tereza Nester, 113
+                <br>
+                Bairro Afonso Pena
+                <br>
+                São José dos Pinhas
+                </a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-12 col-md py-2 py-md-0">
+          <h5>Escola Renovação</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-muted">
+                Rua Altevir de Lara, 705
+                <br>
+                Bairro Afonso Pena
+                <br>
+                São José dos Pinhas
+                </a>
+            </li>
+          </ul>
+        </div>
+    
+        <div class="col-12 col-md py-2 py-md-0">
+          <h5>Contato</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2">
+              <a target="_blank" href="https://wa.me/554196310071" class="nav-link p-0 text-muted">WhatsApp<br/>+55 41 9631-0071</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </div>
 </div>
+<div class="text-muted py-2" style="background: #001233; text-align:center;color:white;">Desenvolvido por Alca</div>
