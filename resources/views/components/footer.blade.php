47a5676b88bf4726ad3bb667e88a55e0b14f265b
bea <beatriztattooer@gmail.com>
2022-09-24 17:18:20 -0300

    :lipstick: quote

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <div style="background: #00153D;color:white;">
     <div class="container">
-      <footer class="row row-cols-5 py-5 mt-5 border-top">
+      <footer class="row row-cols-5 py-5">
         <div class="col-12 col-md py-2 py-md-0">
           <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
           <img src="{{asset('logo/emblema.png')}}">
--- resources/views/components/quote.blade.php
+++ resources/views/components/quote.blade.php
@@ -1,4 +1,20 @@
-<div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
-    quote
+<div class="quote__bg d-flex align-items-center">
+    <div class="container">
+        <div>
+            <h3>
+                “Para cada 1 dólar
+                <br>
+                investido em uma criança,
+                <br>
+                a sociedade ganhará 9”
+            </h3>
+            <small>
+                James Heckman, economista americano
+                <br>
+                ganhador do prêmio Nobel de Economia do ano 2000
+            </small>
+            <br>
+            <button class="btn mt-4">Matricule-se já!</button>
+        </div>
+    </div>
 </div>
