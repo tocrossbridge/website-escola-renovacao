6fec5dcda4a00586c5432b7c3dd9ef496bd9ee1c
bea <beatriztattooer@gmail.com>
2022-09-24 17:52:37 -0300

    :lipstick: style: intro

--- resources/views/components/intro.blade.php
+++ resources/views/components/intro.blade.php
@@ -1,4 +1,7 @@
-<div>
-    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
-    intro
+<div class="intro__bg d-flex align-items-center">
+    <div class="col-12 col-md-6 text-center">
+        <img src="{{ asset('logo/logo-ensino-premium.png') }}" alt="">
+        <p class="my-3">Preparando os protagonistas da nova geração!</p>
+        <img class="intro__icon" src="{{ asset('icons/4pilares-traços.svg') }}" alt="">
+    </div>
 </div>
