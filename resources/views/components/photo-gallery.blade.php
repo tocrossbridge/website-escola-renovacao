771d99e9879d58e97799aab436afcef724609743
bea <beatriztattooer@gmail.com>
2022-09-25 16:38:35 -0300

    adjust: photo-gallery: title
    1. Added a little sun side by side title

--- resources/views/components/photo-gallery.blade.php
+++ resources/views/components/photo-gallery.blade.php
@@ -1,5 +1,12 @@
 <div class="photo-gallery__bg py-5">
     <div class="container">
-        <x-common.title title="Oferecemos uma estrutura preparada<br/>para formar com excelência" />
+        <div class="row">
+            <div class="col-12 col-md-auto">
+                <img class="photo-gallery__title__decoration" src="{{ asset('icons/sol.png') }}" alt="">
+            </div>
+            <div class="col-12 col-md-6 my-auto">
+                <x-common.title title="Oferecemos uma estrutura preparada<br/>para formar com excelência" />
+            </div>
+        </div>
     </div>
 </div>
