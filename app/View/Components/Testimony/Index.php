5526abe4ac96b84640ad0ee19a9293fa47a34871
bea <beatriztattooer@gmail.com>
2022-09-25 16:03:27 -0300

    🚚 fix: testimony
    1. Forgot to commit this content

---
+++ app/View/Components/Testimony/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\View\Components\Testimony;
+
+use Illuminate\View\Component;
+
+class Index extends Component
+{
+
+    public function __construct()
+    {
+        //
+    }
+
+    public function render()
+    {
+        return view('components.testimony.index');
+    }
+}
