75f68bd4aba238e427f1050597fdef51fab1f4eb
bea <beatriztattooer@gmail.com>
2022-09-25 16:03:52 -0300

    🚚 fix: testimony: real data

---
+++ resources/views/components/testimony/index.blade.php
@@ -0,0 +1,11 @@
+<div class="testimony__bg">
+    <div class="container py-5">
+        <x-common.title class="text-center pb-5" title="Buscamos fortalecer laços de amizade,<br>construindo uma comunidade de valor!"/>
+        <div class="row">
+            <x-testimony.testimony image="{{ asset('images/avatar.png') }}" name="landing-page.testimony-01__name" description="landing-page.testimony-01__description" />
+            <x-testimony.testimony image="{{ asset('images/avatar.png') }}" name="landing-page.testimony-02__name" description="landing-page.testimony-02__description" />
+            <x-testimony.testimony image="{{ asset('images/avatar.png') }}" name="landing-page.testimony-03__name" description="landing-page.testimony-03__description" />
+            <x-testimony.testimony image="{{ asset('images/avatar.png') }}" name="landing-page.testimony-04__name" description="landing-page.testimony-04__description" />
+        </div>
+    </div>
+</div>
