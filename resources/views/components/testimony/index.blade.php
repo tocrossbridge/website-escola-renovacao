35c85de7d3f34566e249a38b365aae87c08b9c85
bea <beatriztattooer@gmail.com>
2022-09-25 17:27:25 -0300

    :bento: assets: testimony imgs updated

--- resources/views/components/testimony/index.blade.php
+++ resources/views/components/testimony/index.blade.php
@@ -2,10 +2,10 @@
     <div class="container py-5">
         <x-common.title class="text-center pb-5" title="Buscamos fortalecer laços de amizade,<br>construindo uma comunidade de valor!"/>
         <div class="row">
-            <x-testimony.testimony image="{{ asset('images/avatar.png') }}" name="landing-page.testimony-01__name" description="landing-page.testimony-01__description" />
-            <x-testimony.testimony image="{{ asset('images/avatar.png') }}" name="landing-page.testimony-02__name" description="landing-page.testimony-02__description" />
-            <x-testimony.testimony image="{{ asset('images/avatar.png') }}" name="landing-page.testimony-03__name" description="landing-page.testimony-03__description" />
-            <x-testimony.testimony image="{{ asset('images/avatar.png') }}" name="landing-page.testimony-04__name" description="landing-page.testimony-04__description" />
+            <x-testimony.testimony image="{{ asset('images/testimony-eliana.png') }}" name="landing-page.testimony-01__name" description="landing-page.testimony-01__description" />
+            <x-testimony.testimony image="{{ asset('images/testimony-fernando.png') }}" name="landing-page.testimony-02__name" description="landing-page.testimony-02__description" />
+            <x-testimony.testimony image="{{ asset('images/testimony-giovanna.png') }}" name="landing-page.testimony-03__name" description="landing-page.testimony-03__description" />
+            <x-testimony.testimony image="{{ asset('images/testimony-eduarda.png') }}" name="landing-page.testimony-04__name" description="landing-page.testimony-04__description" />
         </div>
     </div>
 </div>
