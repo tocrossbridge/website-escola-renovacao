ebe29f2c3efaa2bb33e0b8a194990eabd8783863
bea <beatriztattooer@gmail.com>
2022-09-25 16:04:04 -0300

    :speech_balloon: update: lang

---
+++ lang/en/landing-page.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Landing Page
+    |--------------------------------------------------------------------------
+    |
+    | Texto de toda a landing page aqui
+    |
+    */
+
+    // About Us
+    'about-us__title' => 'Conheça o Ensino Renovação em São José dos Pinhais!',
+    'about-us__sub-title' => 'Educação Infantil e Ensino Fundamental',
+    'about-us__text' => 'A quase duas décadas sendo referência em educação em São José dos Pinhais, a Renovação conta com um posicionamento muito claro perante a sociedade. Conheça os 4 pilares que sustentam a instituição:',
+
+    // Testimony
+    'testimony-01__name' => 'Eliana Vaghetti',
+    'testimony-01__description' => 'A escola não só o acolheu mas nos trouxe a paz, pois sabíamos que ali além das brincadeiras e da estrutura que ele tanto gosta, ele aprende todos os dias e nos ensina a ser melhores.',
+
+    'testimony-02__name' => 'Fernando Venante',
+    'testimony-02__description' => 'Amor, segurança, comprometimento,  seriedade, excelência, qualidade,... Eu seria injusto se me limitasse à palavras o que entendo ser a Escola Renovação, seu valor está acima do que se pode mensurar.',
+
+    'testimony-03__name' => 'Giovanna (Aluna Renovação)',
+    'testimony-03__description' => 'Foram anos maravilhosos que passei por aqui! Agradeço a equipe da escola Renovação por transmitir valores Cristãos que hoje em dia são tão importantes!',
+
+    'testimony-04__name' => 'Eduarda do Prado (Ex Aluna)',
+    'testimony-04__description' => 'A Escola Renovação teve um papel fundamental em minha jornada. Foi ali que iniciaram sonhos que pareciam distantes e impossíveis, mas que eram nutridos todos os dias com ensino de qualidade, incentivos, paciência e carinho.',
+
+    // Quote
+    'quote__title' => '“Para cada 1 dólar investido em uma criança, a sociedade ganhará 9”',
+    'quote__small' => 'James Heckman, economista americano<br>ganhador do prêmio Nobel de Economia do ano 2000',
+
+    // General
+    'enroll-now' => 'Matricule-se já!',
+    'secure-your-spot' => 'Garanta sua vaga!',
+
+];
