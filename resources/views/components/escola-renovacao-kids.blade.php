325ddcb1f7904c4fd74b7cdca5dcb52721a700e1
bea <beatriztattooer@gmail.com>
2022-09-26 05:45:02 -0300

    :sparkles: added video

--- resources/views/components/escola-renovacao-kids.blade.php
+++ resources/views/components/escola-renovacao-kids.blade.php
@@ -1,6 +1,7 @@
 <div class="escola-renovacao-kids__bg d-flex align-items-center">
     <div class="col-12 col-md-6 text-center">
         <x-common.title title="landing-page.renovacao-school" />
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/22pHAIgKTp0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <x-common.button class="btn__green" text="landing-page.contact-whatsapp" />
     </div>
 </div>
--- resources/views/components/escola-renovacao.blade.php
+++ resources/views/components/escola-renovacao.blade.php
@@ -1,6 +1,7 @@
 <div class="escola-renovacao__bg d-flex align-items-center flex-direction__desktop">
     <div class="col-12 col-md-6 text-center">
         <x-common.title title="landing-page.renovacao-school" />
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/r0_8IddTHIc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <x-common.button class="btn__green" text="landing-page.contact-whatsapp" />
     </div>
 </div>
