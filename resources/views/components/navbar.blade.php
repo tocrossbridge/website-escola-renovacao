df5780a615184c17627b0da104edd7b94dbc979c
bea <beatriztattooer@gmail.com>
2022-09-26 05:41:17 -0300

    :lipstick: style

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <div class="w-100" style="position: absolute;">
     <div class="container" style="position: relative">
-        <nav class="navbar navbar-expand-lg navbar-light">
+        <nav class="navbar navbar-expand-lg navbar-dark">
         
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -9,15 +9,16 @@
             
             <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
                 <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-                    <li class="nav-item active"><a class="nav-link" href="#">Institucional <span class="sr-only">(current)</span></a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Renovação</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Renovação KIDS</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Matrículas</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Contato</a></li>
+                    <li class="nav-item active"><a class="nav-link mx-2" href="#">Institucional <span class="sr-only">(current)</span></a></li>
+                    <li class="nav-item"><a class="nav-link mx-2" href="#">Renovação</a></li>
+                    <li class="nav-item"><a class="nav-link mx-2" href="#">Renovação KIDS</a></li>
+                    <li class="nav-item"><a class="nav-link mx-2" href="#">Matrículas</a></li>
+                    <li class="nav-item"><a class="nav-link mx-2" href="#">Contato</a></li>
                 </ul>
+
                 <div class="my-2 my-lg-0">
                     <a target="_blank" href="https://renovacao.unimestre.com/projetos/nucleo/uteis/login.php?&tid=0&lid=0&pid=24&arq_ret=R5QT1WSRQBMCVQVPFFQSF99MCT5RT44Q9WRW0RBM0FMM5QQ4R4CV59RWRF1F5SWCW0">
-                        <button class="btn btn-outline-success my-2 my-sm-0">Acesso restrito</button>
+                        <x-common.button class="btn--outlined my-2 my-sm-0" text="Acesso restrito" />
                     </a>
                 </div>
             </div>
