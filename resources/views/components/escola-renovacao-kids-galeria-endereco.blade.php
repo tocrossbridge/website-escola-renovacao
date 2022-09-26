478135764bbf0a017d4ff54df982343b0804f8ee
bea <beatriztattooer@gmail.com>
2022-09-26 20:14:11 -0300

    alteração endereço

--- resources/views/components/escola-renovacao-kids-galeria-endereco.blade.php
+++ resources/views/components/escola-renovacao-kids-galeria-endereco.blade.php
@@ -28,6 +28,6 @@
         </div>
     </div>
     <div class="p-0 col-12 col-md-6">
-        <div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Rua Tereza Nester, 113 Bairro Afonso Pena São José dos Pinhas&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://mcpenation.com/">MCPE Nation</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:400px;}.gmap_iframe {height:400px!important;}</style></div>
+    <div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Rua Altervir de Lara, 705 Bairro Afonso Pena São José dos Pinhas&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://mcpenation.com/">MCPE Nation</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:400px;}.gmap_iframe {height:400px!important;}</style></div>
     </div>
 </div>
