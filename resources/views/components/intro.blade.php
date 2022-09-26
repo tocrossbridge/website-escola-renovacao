c801b8a4f69737e6742da7ac86bb71e1c755cc80
bea <beatriztattooer@gmail.com>
2022-09-26 20:12:41 -0300

    fix intro aguia

--- resources/views/components/intro.blade.php
+++ resources/views/components/intro.blade.php
@@ -1,7 +1,11 @@
-<div class="intro__bg d-flex align-items-center flex-direction__desktop">
-    <div class="col-12 col-md-6 text-center">
-        <img src="{{ asset('logo/logo-ensino-premium.png') }}" alt="">
-        <p class="my-3">Preparando os protagonistas da nova geração!</p>
-        <img class="intro__icon" src="{{ asset('icons/4pilares-traços.svg') }}" alt="">
+<div class="bg--orange">
+    <div class="container">
+        <div class="intro__bg d-flex align-items-center flex-direction__desktop">
+            <div class="col-12 col-md-6 text-center">
+                <img src="{{ asset('logo/logo-ensino-premium.png') }}" alt="">
+                <p class="my-3">Preparando os protagonistas da nova geração!</p>
+                <img class="intro__icon" src="{{ asset('icons/4pilares-traços.svg') }}" alt="">
+            </div>
+        </div>
     </div>
 </div>
