f2286f872580f9c25b60ad43d3f14460101670f9
bea <beatriztattooer@gmail.com>
2022-09-26 20:33:13 -0300

    :bento: photo-gallery: resize imgs

--- resources/views/components/photo-gallery.blade.php
+++ resources/views/components/photo-gallery.blade.php
@@ -15,14 +15,14 @@
                 <div id="imgtext"></div>
             </div>
             <div class="col-12 col-md-6">
-                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/01.jpg') }}" alt="Renovação" onclick="expandThisImg(this);">
-                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/02.jpg') }}" alt="Renovação" onclick="expandThisImg(this);">
-                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/03.jpg') }}" alt="Renovação" onclick="expandThisImg(this);">
-                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/04.jpg') }}" alt="Renovação" onclick="expandThisImg(this);">
-                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/05.jpg') }}" alt="Renovação" onclick="expandThisImg(this);">
-                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/06.jpg') }}" alt="Renovação" onclick="expandThisImg(this);">
-                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/07.jpg') }}" alt="Renovação" onclick="expandThisImg(this);">
-                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/08.jpeg') }}" alt="Renovação" onclick="expandThisImg(this);">
+                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/01.png') }}" alt="Renovação" onclick="expandThisImg(this);">
+                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/02.png') }}" alt="Renovação" onclick="expandThisImg(this);">
+                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/03.png') }}" alt="Renovação" onclick="expandThisImg(this);">
+                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/04.png') }}" alt="Renovação" onclick="expandThisImg(this);">
+                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/05.png') }}" alt="Renovação" onclick="expandThisImg(this);">
+                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/06.png') }}" alt="Renovação" onclick="expandThisImg(this);">
+                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/07.png') }}" alt="Renovação" onclick="expandThisImg(this);">
+                <img class="photo-gallery__mini" src="{{ asset('images/estrutura-escola/08.png') }}" alt="Renovação" onclick="expandThisImg(this);">
 
                 <img class="photo-gallery__mini" src="{{ asset('images/estrutura-kids/01.jpg') }}" alt="Renovação KIDS" onclick="expandThisImg(this);">
                 <img class="photo-gallery__mini" src="{{ asset('images/estrutura-kids/02.jpg') }}" alt="Renovação KIDS" onclick="expandThisImg(this);">
