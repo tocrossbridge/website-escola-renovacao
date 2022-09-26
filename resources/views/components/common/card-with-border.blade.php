48f656be6c43904a25a234c795d173ee15957b60
bea <beatriztattooer@gmail.com>
2022-09-26 19:44:10 -0300

    :lipstick: about-us: icons
    1.  Definição tamanho limite

--- resources/views/components/common/card-with-border.blade.php
+++ resources/views/components/common/card-with-border.blade.php
@@ -1,6 +1,6 @@
 <div class="col-12 col-md-2 d-flex mx-2 text-center border--white">
     <div class="m-auto">
-        <img class="mx--150 mb-3" src="{{ $icon }}" alt="">
+        <img class="mx--150 mb-3 card__icon" src="{{ $icon }}" alt="">
         <p>{!! __($title) !!}</p>
         <p>{!! __($text) !!}</p>
     </div>
