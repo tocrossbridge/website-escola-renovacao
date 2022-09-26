8fc4d6a57b01f377285ca19396a3033d1b52f842
bea <beatriztattooer@gmail.com>
2022-09-26 19:29:35 -0300

    :speech_balloon:  lang

--- resources/views/components/escola-renovacao-kids.blade.php
+++ resources/views/components/escola-renovacao-kids.blade.php
@@ -1,6 +1,6 @@
 <div class="escola-renovacao-kids__bg d-flex align-items-center py-5">
     <div class="col-12 col-md-6 text-center">
-        <x-common.title class="my-4" title="landing-page.renovacao-school" />
+        <x-common.title class="my-4" title="landing-page.renovacao-kids" />
         <iframe width="560" height="315" src="https://www.youtube.com/embed/22pHAIgKTp0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <a target="_blank" href="https://wa.me/554188765100">
             <x-common.button class="btn__green" text="landing-page.contact-whatsapp" />
