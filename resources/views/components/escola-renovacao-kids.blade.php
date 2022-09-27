5e45671cc4e48a54597920b713f91398f4bbf8f9
bea <beatriztattooer@gmail.com>
2022-09-26 21:23:19 -0300

    :lipstick: ajustes video

--- resources/views/components/escola-renovacao-kids.blade.php
+++ resources/views/components/escola-renovacao-kids.blade.php
@@ -1,7 +1,9 @@
 <div id="renovacao-kids" class="escola-renovacao-kids__bg d-flex align-items-center py-5">
     <div class="col-12 col-md-6 text-center">
         <x-common.title class="my-4" title="landing-page.renovacao-kids" />
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/22pHAIgKTp0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div class="container-iframe">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/22pHAIgKTp0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
         <br>
         <a target="_blank" href="https://wa.me/554188765100">
             <x-common.button-whats />
