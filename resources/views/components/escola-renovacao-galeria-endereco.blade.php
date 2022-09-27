d43b2d24072aa0c1e9bfe25f5fcf4e3ff61e419a
bea <beatriztattooer@gmail.com>
2022-09-27 16:18:16 -0300

    :lipstick:

--- resources/views/components/escola-renovacao-galeria-endereco.blade.php
+++ resources/views/components/escola-renovacao-galeria-endereco.blade.php
@@ -1,4 +1,4 @@
-<div class="m-0 row bg--light-yellow">
+<div class="m-0 row bg--orange">
     <div class="p-0 col-12 col-md-6">
         <div class="responsive-renovacao custom-slider">
             <div>
